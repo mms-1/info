59b124dcc78487ec4eb2a7b9960d1c47c9deb36c
Maksim Mikhalchuk <maksim.mikhalchuk@gmail.com>
2017-04-27 23:04:50 +0300

    Added firebase connection

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,19 @@
  * index.js
  * - All our useful JS goes here, awesome!
  */
+function init() {
+    // Initialize Firebase
+    // TODO: Replace with your project's customized code snippet
+    var config = {
+        apiKey: "AIzaSyCrVTABkv3qyrvVZIJZSg_GhlUudo6pdD4",
+        authDomain: "palaml-e4773.firebaseapp.com",
+        databaseURL: "https://palaml-e4773.firebaseio.com",
+        projectId: "palaml-e4773",
+        storageBucket: "palaml-e4773.appspot.com",
+        messagingSenderId: "209158373491"
+    };
+    firebase.initializeApp(config);
+    console.log("JavaScript is amazing!");
+};
 
-console.log("JavaScript is amazing!");
+init();
